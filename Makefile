gleegaye/AstroMatt_book-devops

Book: Makefile and requirements #time 10m

Makefile
@@ -1,5 +1,5 @@
-book = /tmp/devops-book/
-slides = /tmp/devops-slides/
+book = /tmp/book-devops/
+slides = /tmp/slides-devops/
 cpu_cores = 11
 format = html
 

easyMil/book-python

Makefile
@@ -1,5 +1,5 @@
-book = /tmp/python-book/
-slides = /tmp/python-slides/
+book = /tmp/book-python/
+slides = /tmp/slides-python/
 cpu_cores = 11
 format = html
 